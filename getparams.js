128f5338638bcf5685047b23f5500c3eb03c1889
etangaming123 <etangaming123@gmail.com>
2025-03-10 17:10:49 +1100

    Fix dynamic text script
    Added auto capitalisation

--- getparams.js
+++ getparams.js
@@ -3,8 +3,9 @@ const fromParam = params.get('from'); // Get specific param
 
 const dymanicTextElement = document.getElementById("dynamic-text-element"); // Get the element to change
 
-const visitsFrom = ["Discord", "Youtube", "GitHub", "Instagram"] // Avaliable visit pages
+const visitsFrom = ["discord", "youtube", "github", "instagram"] // Avaliable visit pages
 
 if (fromParam && visitsFrom.includes(fromParam)) { // If the param exists and it is in the array "visitsFrom"
-    dymanicTextElement.textContent = `I see you're visiting from ${fromParam}!` // Change the text
+    const userVisitedFrom = fromParam.charAt(0).toUpperCase() + fromParam.slice(1) // Capitalise word
+    dymanicTextElement.textContent = `I see you're visiting from ${userVisitedFrom}!` // Change the text
 }
