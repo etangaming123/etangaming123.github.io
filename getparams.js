0ee72a740689b0afec1dc0c7749da9c87e1cfa0f
etangaming123 <etangaming123@gmail.com>
2025-03-10 17:15:58 +1100

    Various fixes
    Fixed url redirect bug and added logging of params

--- getparams.js
+++ getparams.js
@@ -5,6 +5,8 @@ const dymanicTextElement = document.getElementById("dynamic-text-element"); // G
 
 const visitsFrom = ["discord", "youtube", "github", "instagram"] // Avaliable visit pages
 
+console.log(params)
+
 if (fromParam && visitsFrom.includes(fromParam)) { // If the param exists and it is in the array "visitsFrom"
     const userVisitedFrom = fromParam.charAt(0).toUpperCase() + fromParam.slice(1) // Capitalise word
     dymanicTextElement.textContent = `I see you're visiting from ${userVisitedFrom}!` // Change the text
