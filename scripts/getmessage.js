2ff1cfbd4b7728d26be47b43e042b93041f0c949
etangaming123 <etangaming123@gmail.com>
2025-03-11 22:37:18 +1100

    you dont need to know about this one

---
+++ scripts/getmessage.js
@@ -0,0 +1,24 @@
+const params = new URLSearchParams(window.location.search); // Get params
+const messageParam = params.get('message'); // Get specific param
+
+const messageElement = document.getElementById("message"); // Get the element to change
+const statusElement = document.getElementById("status"); 
+
+async function replaceText(id) {
+    try {
+        const response = await fetch(`https://gist.githubusercontent.com/etangaming123/${id}/raw`) // MY gists thank you very much >:P
+        if (!response.ok) {
+            messageElement.textContent = `Failed to get message! Status: ${response.status}`
+            return
+        }
+        messageElement.textContent = await response.text();
+        statusElement.innerHTML = `Message from <a href=https://gist.githubusercontent.com/etangaming123/${id}/raw class="link">https://gist.githubusercontent.com/etangaming123/${id}/raw</a>`;
+    }
+    catch (error) {
+        console.error("Failed to fetch message:", error)
+    }
+}
+
+if (messageParam) { // If the param exists
+    replaceText(messageParam)
+}
