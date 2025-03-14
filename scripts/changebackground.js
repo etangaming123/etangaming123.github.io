18944e582cb99b06af11ae28521922163d7b8f6e
etangaming123 <etangaming123@gmail.com>
2025-03-14 14:51:43 +1100

    character update + fixes

---
+++ scripts/changebackground.js
@@ -0,0 +1,32 @@
+async function setUserBg(color) {
+    statustext = document.getElementById("status-text")
+    if (color == "none") {
+        localStorage.removeItem("userbg");
+    }
+    else {
+        localStorage.setItem("userbg", color);
+    }
+    const scrollingBackground = document.querySelector(".scrolling-background");
+
+    userbg = localStorage.getItem("userbg")
+
+    if (userbg == null) {
+        const gistUrl = "https://gist.githubusercontent.com/etangaming123/071d58869981c2a5a56c2f3ff11aebc9/raw";
+
+        try {
+            const response = await fetch(gistUrl);
+            const fileName = await response.text();
+    
+            scrollingBackground.style.backgroundImage = `url('./images/backgrounds/${fileName}')`;
+        } catch (error) {
+            console.error("Error fetching Gist data:", error);
+            scrollingBackground.style.backgroundImage = `url('./images/backgrounds/bg.png')`;
+        }    
+    }
+
+    else {
+        scrollingBackground.style.backgroundImage = `url('./images/backgrounds/${userbg}bg.png')`;
+    }
+
+    statustext.innerText = `Set background to ${color}!`
+}
