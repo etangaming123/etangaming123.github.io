4aa76400066100a86027cd20434f2292da1d4cf2
etangaming123 <etangaming123@gmail.com>
2025-05-09 14:56:27 +1000

    message updates

--- scripts/getmessage.js
+++ scripts/getmessage.js
@@ -9,7 +9,7 @@ async function replaceText(id) {
         const response = await fetch(`https://gist.githubusercontent.com/etangaming123/${id}/raw`) // MY gists thank you very much >:P
         if (!response.ok) {
             if (response.status == 404) {
-                messageElement.textContent = "The message id you inputted either does not exist or has been deleted."
+                messageElement.textContent = "The message id you inputted does not exist. (if it worked before, it may have been deleted!)"
                 return
             }
             messageElement.textContent = `Failed to get message! Status: ${response.status}`
@@ -19,8 +19,9 @@ async function replaceText(id) {
         if (/^https?:\/\/\S+$/.test(text)) {
             window.location.href = text;
         }    
-        messageElement.innerText = text.replace("/\n\g", "\n")
-        statusElement.innerHTML = `Message from <a href=https://gist.githubusercontent.com/etangaming123/${id}/raw class="external-link">this link</a>`;
+        const html = text.replace(/(https?:\/\/[\w\-.~:?#[\]@!$&'()*+,;=\/]+)(?![^<]*?>)/g, '<a href="$1" class="external-link" target="_blank">$1</a>');
+        messageElement.innerHTML = `<pre style="white-space: pre-wrap;">${html}</pre>`
+        statusElement.innerHTML = `<a href=https://gist.githubusercontent.com/etangaming123/${id}/raw class="external-link" target="_blank">View raw</a> // <a href=https://gist.githubusercontent.com/etangaming123/${id}/revisions class="external-link" target="_blank">View update history</a>`;
     }
     catch (error) {
         console.error("Failed to fetch message:", error)
