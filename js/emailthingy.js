1de5fe4d8352116a7c2ec392868169c8d424a6be
etangaming123 <etangaming123@gmail.com>
2026-03-09 08:55:18 +1100

    email fixes

--- js/emailthingy.js
+++
@@ -1,5 +0,0 @@
-const emailtextelement = document.getElementById("waht");
-const email = "me@etangaming.xyz";
-
-emailtextelement.href = "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=" + email;
-emailtextelement.textContent = email;
